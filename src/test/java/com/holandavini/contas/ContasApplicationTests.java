d6ecb1375f6e7f786f1683db64a25f556f6148af
Vinicius Holanda <viniciusholanda93@gmail.com>
2024-06-12 21:05:21 -0300

    Primeiro

---
+++ src/test/java/com/holandavini/contas/ContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holandavini.contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
